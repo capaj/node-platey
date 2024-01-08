7577893f291a4bdc35cb9af7f2cbaac70c773a9e
Jiří Špác <capajj@gmail.com>
2024-01-08 02:22:14 +0100

    fix missing var

--- bin.cjs
+++ bin.cjs
@@ -4,7 +4,7 @@ const { spawnSync } = require("node:child_process");
 
 const path = require("path");
 
-const tsxPath = path.join(currentDir, "node_modules", "tsx");
+const tsxPath = path.join(__dirname, "node_modules", "tsx");
 console.log("tsxPath:", tsxPath);
 // run in tsx
 spawnSync(
