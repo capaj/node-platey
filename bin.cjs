3177166ddfea5923063ce3ccb09a6e05d47dc64d
Jiří Špác <capajj@gmail.com>
2024-01-08 02:59:20 +0100

    fix tsx comman execution

--- bin.cjs
+++ bin.cjs
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 
-const { spawnSync } = require("node:child_process");
+const { spawnSync } = require('node:child_process')
 
-const path = require("path");
+const path = require('path')
 
-const tsxPath = path.join(__dirname, "node_modules", "tsx");
-console.log("tsxPath:", tsxPath);
 // run in tsx
 spawnSync(
-  "node",
-  ["--import", tsxPath, path.join(__dirname, "nodePlatey.ts"), process.argv[2]],
+  'npx',
+  ['-y', 'tsx', path.join(__dirname, 'nodePlatey.ts'), process.argv[2]],
   {
-    stdio: "inherit",
-  },
-);
+    stdio: 'inherit'
+  }
+)
