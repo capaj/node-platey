e4e4961dd116ae61696e674fe57c149ceec49273
Jiří Špác <capajj@gmail.com>
2024-01-08 01:34:59 +0100

    use tsx instead of ts-node

--- bin.cjs
+++ bin.cjs
@@ -1,18 +1,14 @@
 #!/usr/bin/env node
 
-const { spawnSync } = require('node:child_process')
+const { spawnSync } = require("node:child_process");
 
-const path = require('path')
+const path = require("path");
 
+// run in tsx
 spawnSync(
-  'ts-node-esm',
-  [
-    '--experimental-specifier-resolution=node',
-    '--transpileOnly',
-    path.join(__dirname, 'nodePlatey.ts'),
-    process.argv[2]
-  ],
+  "node",
+  ["--import", "tsx", path.join(__dirname, "nodePlatey.ts"), process.argv[2]],
   {
-    stdio: 'inherit'
-  }
-)
+    stdio: "inherit",
+  },
+);
